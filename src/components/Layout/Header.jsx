0486c6567c9f1e69eb4fa931189283953fe00a41
Altynai <altinaikacool@gmail.com>
2022-10-31 16:20:37 +0600

    finished header part

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import mealsImage from "./../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderButton from "./HeaderButton";
+function Header() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderButton/>
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="food" />
+      </div>
+    </>
+  );
+}
+export default Header;
---
+++ src/components/Layout/HeaderButton.jsx
@@ -0,0 +1,15 @@
+import classes from "./HeaderButton.module.css"
+import CardIcon from "../Icons/CardIcon";
+function HeaderButton (){
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CardIcon/>
+            </span>
+
+            <span>Your Card</span>
+            <span>3</span>
+        </button>
+    )
+}
+export default HeaderButton;
